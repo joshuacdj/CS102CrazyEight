0fdcbd39f3b0724c81ee29776d2b0a2eaf384124
joshuacdj <joshuacheungdj@gmail.com>
2024-03-01 09:22:01 +0800

    New IntelliJ Project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module org.example.cs102proj {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens org.example.cs102proj to javafx.fxml;
+    exports org.example.cs102proj;
+}
