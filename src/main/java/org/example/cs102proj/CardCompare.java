0fe93232ee42220bb62818a66cb8548772646cac
derej_gnow <jeredwong711@gmail.com>
2024-03-04 10:03:08 +0800

    Modified CardCompare class
    changed order of card value to ascending

--- src/main/java/org/example/cs102proj/CardCompare.java
+++ src/main/java/org/example/cs102proj/CardCompare.java
@@ -4,12 +4,7 @@ import java.util.Comparator;
 
 public class CardCompare implements Comparator<Card> {
     public int compare(Card a, Card b) {
-        if (a.getValue() > b.getValue()) {
-            return -1; // highest value first
-        }
-        if (a.getValue() < b.getValue()) {
-            return 1; // highest value first
-        }
-        return 0;
+//        cards in ascending order
+        return a.getValue() - b.getValue();
     }
 }
