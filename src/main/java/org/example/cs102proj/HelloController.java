791bb56f47abc4c1e31b8d6a08010332fbdbe1e5
derej_gnow <jeredwong711@gmail.com>
2024-03-11 14:13:00 +0800

    Added Controller skeleton

--- src/main/java/org/example/cs102proj/HelloController.java
+++ src/main/java/org/example/cs102proj/HelloController.java
@@ -2,6 +2,7 @@ package org.example.cs102proj;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
 
 public class HelloController {
     @FXML
@@ -10,5 +11,13 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void draw(ActionEvent e) {
+        System.out.println("draw card");
+    }
+
+    public void onStartGameClick(ActionEvent e) {
+        System.out.println("start game");
     }
 }
