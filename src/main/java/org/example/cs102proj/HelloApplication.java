020e84af1510afd775d867ef34bd72aa596bb847
joshuacdj <joshuacheungdj@gmail.com>
2024-03-18 10:52:03 +0800

    added Changes with javafx

--- src/main/java/org/example/cs102proj/HelloApplication.java
+++ src/main/java/org/example/cs102proj/HelloApplication.java
@@ -13,12 +13,17 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("TitleScreen.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400.5);
+        stage.setTitle("Crazy Eights!");
         stage.setScene(scene);
         stage.show();
     }
-
     public static void main(String[] args) {
 
         launch();
     }
 }
+
+
+
--- src/main/java/org/example/cs102proj/HelloController.java
+++ src/main/java/org/example/cs102proj/HelloController.java
@@ -12,3 +12,5 @@ public class HelloController {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
+
+
